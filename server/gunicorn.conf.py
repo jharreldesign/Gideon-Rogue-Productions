4299700ec2efc4d435330f1bf6fc62d65756a7c2
jharreldesign <harrel.jason@gmail.com>
2024-12-16 17:12:18 -0800

    Updates to backend

--- server/gunicorn.conf.py
+++ server/gunicorn.conf.py
@@ -20,24 +20,3 @@ timeout = 30  # Restart workers if they hang for more than 30 seconds
 reload = False  # Set to True for development to auto-reload workers when code changes
 
 
-
-
-# # gunicorn.conf.py
-
-# # Server socket
-# bind = "0.0.0.0:5000"  # Binds to all available IPs on port 5000
-
-# # Workers
-# workers = 4  # Number of worker processes
-# worker_class = "sync"  # Type of workers; sync is the default
-
-# # Logging
-# accesslog = "-"  # Log access to stdout
-# errorlog = "-"   # Log errors to stdout
-# loglevel = "info"  # Logging level (debug, info, warning, error, critical)
-
-# # Timeout
-# timeout = 30  # Restart workers if they hang for more than 30 seconds
-
-# # Reload
-# reload = True  # Automatically reload workers when code changes (useful for dev)
